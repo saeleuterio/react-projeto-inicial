c35a2483b2ecadbb9bfaa637e42012c697eaba93
saeleuterio <saeleuterio@hotmail.com>
2026-08-26 11:09:39 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,64 @@
-import { ChecklistsWrapper } from "./components/ChecklistsWrapper"
-import { Container } from "./components/Container"
-import { Dialog } from "./components/Dialog"
-import { FabButton } from "./components/FabButton"
-import { Footer } from "./components/Footer"
-import { Header } from "./components/Header"
-import { Heading } from "./components/Heading"
-import { IconPlus, IconSchool } from "./components/icons"
-import { SubHeading } from "./components/SubHeading"
-import { ToDoItem } from "./components/ToDoItem"
-import { ToDoList } from "./components/ToDoList"
+import { useState } from "react";
+import { ChecklistsWrapper } from "./components/ChecklistsWrapper";
+import { Container } from "./components/Container";
+import { Dialog } from "./components/Dialog";
+import { FabButton } from "./components/FabButton";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Heading } from "./components/Heading";
+import { IconPlus, IconSchool } from "./components/icons";
+import { SubHeading } from "./components/SubHeading";
+import { ToDoItem } from "./components/ToDoItem";
+import { ToDoList } from "./components/ToDoList";
 
 const todos = [
   {
     id: 1,
     description: "JSX e componentes",
     completed: false,
-    createdAt: "2022-10-31"
+    createdAt: "2022-10-31",
   },
   {
     id: 2,
     description: "Props, state e hooks",
     completed: false,
-    createdAt: "2022-10-31"
+    createdAt: "2022-10-31",
   },
   {
     id: 3,
     description: "Ciclo de vida dos componentes",
     completed: false,
-    createdAt: "2022-10-31"
+    createdAt: "2022-10-31",
   },
   {
     id: 4,
     description: "Testes unitários com Jest",
     completed: false,
-    createdAt: "2022-10-31"
-  }
-]
+    createdAt: "2022-10-31",
+  },
+];
 const completed = [
   {
     id: 5,
     description: "Controle de inputs e formulários controlados",
     completed: true,
-    createdAt: "2022-10-31"
+    createdAt: "2022-10-31",
   },
   {
     id: 6,
     description: "Rotas dinâmicas",
     completed: true,
-    createdAt: "2022-10-31"
-  }
-]
+    createdAt: "2022-10-31",
+  },
+];
 
 function App() {
+  const [showDialog, setShowDialog] = useState(false)
+
+  const toggleDialog = () => {
+    setShowDialog(!showDialog)
+    console.log('alternar modal')
+  };
 
   return (
     <main>
@@ -61,7 +68,6 @@ function App() {
             <IconSchool /> Plano de estudos
           </Heading>
         </Header>
-        <Dialog/>
         <ChecklistsWrapper>
           <SubHeading>Para estudar</SubHeading>
           <ToDoList>
@@ -76,14 +82,15 @@ function App() {
             })}
           </ToDoList>
           <Footer>
-            <FabButton>
+            <Dialog isOpen={showDialog} onClose={toggleDialog}/>
+            <FabButton onClick={toggleDialog}>
               <IconPlus />
             </FabButton>
           </Footer>
         </ChecklistsWrapper>
       </Container>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,12 +1,21 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./dialog.style.css";
 
-export function Dialog() {
+export function Dialog({ isOpen, onClose }) {
     // não deveriamos fazer buscas no DOM desse jeito!
     // const dialog = document.querySelector("dialog");
 
     const dialogRef = useRef (null)
+
+    useEffect(() => {
+      console.log('Deveríamos mostrar a modal?', isOpen)
+      if (isOpen) {
+        openDialog()
+      } else {
+        closeDialog()
+      }
+    }, [isOpen])
 
     // "Show the dialog" button opens the dialog modally
     const openDialog = () => {
@@ -21,7 +30,7 @@ export function Dialog() {
   return (
     <React.Fragment>
       <dialog ref={dialogRef}>
-        <button autoFocus onClick={closeDialog}>Close</button>
+        <button autoFocus onClick={onClose}>Close</button>
         <p>This modal dialog has a groovy backdrop!</p>
       </dialog>
       <button onClick={openDialog}>Show the dialog</button>
